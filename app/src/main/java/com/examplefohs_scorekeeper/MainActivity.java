1b1ef00dcf78c789cb47b00cf28a2351c4f8e252
Ian Worley <Therealianw@protonmail.com>
2021-01-13 10:53:42 -0800

    This might fix it......

--- app/src/main/java/com/examplefohs_scorekeeper/MainActivity.java
+++ app/src/main/java/com/examplefohs_scorekeeper/MainActivity.java
@@ -16,7 +16,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { 
 
     private AppBarConfiguration mAppBarConfiguration;
 
